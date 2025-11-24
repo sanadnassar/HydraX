b0db1572a17dc26f5c77fb93ca01db90ac1c0bef
sanadnassar <194846666+sanadnassar@users.noreply.github.com>
2025-11-24 14:42:37 -0500

    Added environment variable

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,7 @@
 # backend/config.py
 import os
-from secret import GOOGLE_MAPS_API_KEY
+
+GOOGLE_MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 
 class Settings:
     GOOGLE_MAPS_API_KEY = GOOGLE_MAPS_API_KEY
